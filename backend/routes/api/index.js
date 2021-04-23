adedefeb282232336ffb2edfdf8a49e9405aef18
Nick Bierman <biermannick83@gmail.com>
2021-04-23 12:47:50 -0400

    add POST /products/like/:id route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,18 +1,27 @@
 const router = require("express").Router();
 const db = require('../../db/models');
+const csurf = require("csurf");
 const { Product } = db;
 
 router.get("/products", async function (req, res) {
     const products = await Product.findAll();
-    res.cookie("XSRF-TOKEN", req.csrfToken());
+    // res.cookie("XSRF-TOKEN", req.csrfToken());
     res.send(products);
 });
 
 router.get('/products/:id', async function (req, res) {
     const id = parseInt(req.params.id);
     const product = await Product.findByPk(id);
-    res.cookie("XSRF-TOKEN", req.csrfToken());
+    // res.cookie("XSRF-TOKEN", req.csrfToken());
     res.send(product);
+});
+
+router.post('/products/like/:id', async function (req, res) {
+    const id = parseInt(req.params.id);
+    const product = await Product.findByPk(id);
+    product.num_likes++;
+    await product.save();
+    res.json(product);
 });
 
 module.exports = router;
