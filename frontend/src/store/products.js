affeb885dd6d3b678e270a7f88c948d569ef183b
Nick Bierman <biermannick83@gmail.com>
2021-04-26 10:49:24 -0400

    initial react setup

---
+++ frontend/src/store/products.js
@@ -0,0 +1,27 @@
+const SET_PRODUCTS = 'products/setProducts';
+
+const setProducts = (products) => ({
+    type: SET_PRODUCTS,
+    payload: products
+});
+
+export const fetchProducts = () => async dispatch => {
+    const response = await fetch('/api/products');
+    dispatch(setProducts(response.data));
+    return response;
+};
+
+const initialState = { products: [] };
+
+function reducer(state = initialState, action) {
+    let newState;
+    switch (action.type) {
+        case SET_PRODUCTS:
+            newState = { ...state, products: action.payload };
+            return newState;
+        default:
+            return state;
+    };
+};
+
+export default reducer;
